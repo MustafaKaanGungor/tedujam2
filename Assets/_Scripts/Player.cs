6d5e9839e0d5d175f05e61c965f8222088f64913
Mustafa Kaan Güngör <kaan062004+github@gmail.com>
2025-02-21 23:31:13 +0300

    added basic movement, needs fixing

--- Assets/_Scripts/Player.cs
+++ Assets/_Scripts/Player.cs
@@ -2,15 +2,47 @@ using UnityEngine;
 
 public class Player : MonoBehaviour
 {
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
-    void Start()
-    {
-        
+    public static Player Instance {get; private set;}
+    private Rigidbody2D playerRb;
+
+    float rotationAngle = 0f;
+    [SerializeField] private float speedFactor = 30f;
+    [SerializeField] private float turnFactor = 3.5f;
+
+    private void Awake() {
+        Instance = this;
+
+        playerRb = GetComponent<Rigidbody2D>();
     }
 
-    // Update is called once per frame
-    void Update()
-    {
-        
+    private void FixedUpdate() {
+        ForwardMovement();
+        TurningMovement();
+    }
+
+    private void ForwardMovement() {
+
+        if (GameInput.Instance.GetMovementVector().x == 0)
+        {
+            playerRb.linearDamping = Mathf.Lerp(playerRb.linearDamping, 3.0f, Time.fixedDeltaTime * 3);
+        }
+        else
+        {
+            playerRb.linearDamping = 0;
+        }
+
+        Vector2 swimmingVector = transform.up * GameInput.Instance.GetMovementVector().y * speedFactor;
+    
+        playerRb.AddForce(swimmingVector * Time.deltaTime, ForceMode2D.Force);
+    }
+
+    private void TurningMovement() {
+        /*
+        rotationAngle -= GameInput.Instance.GetMovementVector().x * turnFactor;
+
+        playerRb.MoveRotation(rotationAngle);
+        */
+
+        transform.Rotate(0, 0, GameInput.Instance.GetMovementVector().x * turnFactor * Time.deltaTime);
     }
 }
